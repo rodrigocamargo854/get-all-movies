b19e88cd5c17e03944c297a3e75fe715b9d9143f
Rodrigo Correa de Godoy Camargo <rodrigo.godoy@hbsis.com.br>
2022-08-31 09:24:42 -0300

    fixed path name

--- pages/treading/index.tsx
+++ pages/treading/index.tsx
@@ -5,6 +5,7 @@ import {
   Box,
   SimpleGrid,
   CircularProgress,
+  Stack,
 } from "@chakra-ui/react";
 
 const defaultEndPoint = `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.NEXT_PUBLIC_SYSTEM_ID}`;
@@ -29,28 +30,37 @@ export default function Treading({ data }) {
         <Text>Movies From TMDB </Text>
       </Flex>
       <Flex align="center" justify="center">
-        <SimpleGrid columns={3} spacing={5}>
+        <SimpleGrid columns={3} spacing={4}>
           {results.map((result) => {
-            const { id, title, poster_path, vote_average } =
-              result;
+            const { id, poster_path, vote_average } = result;
             const image = `https://image.tmdb.org/t/p/w200/${poster_path}`;
 
             return (
               <Box key={id}>
-                <img src={image} alt={title} />
+                <img src={image} />
                 <Text
-                  fontSize="2xl"
+                  fontSize="1xl"
                   fontWeight="bold"
                   letterSpacing="tight"
                   color="yellow.500"
                   w="64"
                 >
-                  {title}
+                  Name: {result?.name || result?.title}
                 </Text>
-                <Text>Date: {result?.release_date || result?.first_air_date}</Text>
-                <CircularProgress value={vote_average}>
-                  {vote_average}
-                </CircularProgress>
+                <Text
+                  fontSize="1xl"
+                  fontWeight="bold"
+                  letterSpacing="tight"
+                  color="pink.500"
+                  w="64"
+                >
+                  Date: {result?.release_date || result?.first_air_date}
+                </Text>
+                <Stack spacing="410">
+                  <CircularProgress value={vote_average} size="20px">
+                    {vote_average}
+                  </CircularProgress>
+                </Stack>
               </Box>
             );
           })}
